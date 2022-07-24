e7c18dcb62bd80112f3a0940631622c213ba6305
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-07-24 20:17:59 -0300

    Start project and setting up firebase

---
+++ firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINSENDERID,
+    appId: process.env.APPID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth }
